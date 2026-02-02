695dc7d3014f10d70611ab4177a7149d5d9c74f8
DevExpressExampleBot <fakeEmail>
2026-02-02 22:53:25 +0400

    Source auto update [skip ci]

--- VB/XtraDiagram.CustomDraw/Form1.Designer.vb
+++ VB/XtraDiagram.CustomDraw/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace XtraDiagram.CustomDraw
-    Partial Public Class Form1
+Namespace XtraDiagram.CustomDraw
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -26,7 +28,7 @@
             Me.components = New System.ComponentModel.Container()
             Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
             Me.diagramControl1 = New DevExpress.XtraDiagram.DiagramControl()
-            DirectCast(Me.diagramControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.diagramControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' defaultLookAndFeel1
@@ -38,10 +40,12 @@
             Me.diagramControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.diagramControl1.Location = New System.Drawing.Point(0, 0)
             Me.diagramControl1.Name = "diagramControl1"
-            Me.diagramControl1.OptionsBehavior.SelectedStencils = New DevExpress.Diagram.Core.StencilCollection(New String() { "BasicShapes", "BasicFlowchartShapes"})
+            Me.diagramControl1.OptionsBehavior.SelectedStencils = New DevExpress.Diagram.Core.StencilCollection(New String() {"BasicShapes", "BasicFlowchartShapes"})
             Me.diagramControl1.Size = New System.Drawing.Size(907, 580)
             Me.diagramControl1.TabIndex = 0
             Me.diagramControl1.Text = "diagramControl1"
+            AddHandler Me.diagramControl1.CustomGetEditableItemProperties, New System.EventHandler(Of DevExpress.XtraDiagram.DiagramCustomGetEditableItemPropertiesEventArgs)(AddressOf Me.diagramControl1_CustomGetEditableItemProperties)
+            AddHandler Me.diagramControl1.CustomDrawItem, New System.EventHandler(Of DevExpress.XtraDiagram.CustomDrawItemEventArgs)(AddressOf Me.diagramControl1_CustomDrawItem)
             ' 
             ' Form1
             ' 
@@ -51,14 +55,13 @@
             Me.Controls.Add(Me.diagramControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.diagramControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.diagramControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+#End Region
         Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
-        Private WithEvents diagramControl1 As DevExpress.XtraDiagram.DiagramControl
+
+        Private diagramControl1 As DevExpress.XtraDiagram.DiagramControl
     End Class
 End Namespace
-
--- VB/XtraDiagram.CustomDraw/Form1.vb
+++ VB/XtraDiagram.CustomDraw/Form1.vb
@@ -1,18 +1,13 @@
-﻿Imports DevExpress.Diagram.Core
+Imports DevExpress.Diagram.Core
 Imports DevExpress.Utils.Serializing
 Imports DevExpress.XtraDiagram
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows.Forms
 
 Namespace XtraDiagram.CustomDraw
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
         Public Sub New()
@@ -25,21 +20,19 @@ Namespace XtraDiagram.CustomDraw
         Public Sub RegisterShapes()
             DiagramControl.ItemTypeRegistrator.Register(GetType(DiagramShapeEx))
             Dim stencil = New DiagramStencil("customShapes", "Custom Shapes")
-            stencil.RegisterTool(New FactoryItemTool("activeTaskShape", Function() "Active Task", Function(diagram) New DiagramShapeEx With {.Content = "Active Task", .Status = Status.Active}, New System.Windows.Size(150, 100), False))
-            stencil.RegisterTool(New FactoryItemTool("inactiveTaskShape", Function() "Inactive Task", Function(diagram) New DiagramShapeEx With {.Content = "Inactive Task", .Status = Status.Inactive}, New System.Windows.Size(150, 100), False))
+            stencil.RegisterTool(New FactoryItemTool("activeTaskShape", Function() "Active Task", Function(diagram) New DiagramShapeEx With {.Content = "Active Task", .Status = Status.Active}, New Windows.Size(150, 100), False))
+            stencil.RegisterTool(New FactoryItemTool("inactiveTaskShape", Function() "Inactive Task", Function(diagram) New DiagramShapeEx With {.Content = "Inactive Task", .Status = Status.Inactive}, New Windows.Size(150, 100), False))
             DiagramToolboxRegistrator.RegisterStencil(stencil)
             diagramControl1.OptionsBehavior.SelectedStencils = StencilCollection.Parse("customShapes")
         End Sub
 
-        Private Sub diagramControl1_CustomGetEditableItemProperties(ByVal sender As Object, ByVal e As DiagramCustomGetEditableItemPropertiesEventArgs) Handles diagramControl1.CustomGetEditableItemProperties
+        Private Sub diagramControl1_CustomGetEditableItemProperties(ByVal sender As Object, ByVal e As DiagramCustomGetEditableItemPropertiesEventArgs)
             e.Properties.Add(TypeDescriptor.GetProperties(GetType(DiagramShapeEx))("Status"))
         End Sub
 
-        Private Sub diagramControl1_CustomDrawItem(ByVal sender As Object, ByVal e As CustomDrawItemEventArgs) Handles diagramControl1.CustomDrawItem
+        Private Sub diagramControl1_CustomDrawItem(ByVal sender As Object, ByVal e As CustomDrawItemEventArgs)
             Dim shape = TryCast(e.Item, DiagramShapeEx)
-            If shape Is Nothing Then
-                Return
-            End If
+            If shape Is Nothing Then Return
             e.DefaultDraw()
             Dim imagePath = If(shape.Status = Status.Active, "images/actions/apply_16x16.png", "images/actions/cancel_16x16.png")
             Dim image = DevExpress.Images.ImageResourceCache.Default.GetImage(imagePath)
@@ -52,8 +45,8 @@ Namespace XtraDiagram.CustomDraw
     Public Class DiagramShapeEx
         Inherits DiagramShape
 
-        <XtraSerializableProperty, Category("Info")> _
-        Public Property Status() As Status
+        <XtraSerializableProperty, Category("Info")>
+        Public Property Status As Status
     End Class
 
     Public Enum Status
