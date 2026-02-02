17eb0b631c08b664700dded387e6aa435266f980
DevExpressExampleBot <fakeEmail>
2026-02-02 22:53:50 +0400

    Source auto update [skip ci]

--- VB/XtraDiagram.CustomDraw/Form1.vb
+++ VB/XtraDiagram.CustomDraw/Form1.vb
@@ -1,18 +1,12 @@
-﻿Imports DevExpress.Diagram.Core
+Imports DevExpress.Diagram.Core
 Imports DevExpress.Utils.Serializing
 Imports DevExpress.XtraDiagram
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows.Forms
 
 Namespace XtraDiagram.CustomDraw
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
         Public Sub New()
@@ -31,15 +25,13 @@ Namespace XtraDiagram.CustomDraw
             diagramControl1.OptionsBehavior.SelectedStencils = StencilCollection.Parse("customShapes")
         End Sub
 
-        Private Sub diagramControl1_CustomGetEditableItemProperties(ByVal sender As Object, ByVal e As DiagramCustomGetEditableItemPropertiesEventArgs) Handles diagramControl1.CustomGetEditableItemProperties
+        Private Sub diagramControl1_CustomGetEditableItemProperties(ByVal sender As Object, ByVal e As DiagramCustomGetEditableItemPropertiesEventArgs)
             e.Properties.Add(TypeDescriptor.GetProperties(GetType(DiagramShapeEx))("Status"))
         End Sub
 
-        Private Sub diagramControl1_CustomDrawItem(ByVal sender As Object, ByVal e As CustomDrawItemEventArgs) Handles diagramControl1.CustomDrawItem
+        Private Sub diagramControl1_CustomDrawItem(ByVal sender As Object, ByVal e As CustomDrawItemEventArgs)
             Dim shape = TryCast(e.Item, DiagramShapeEx)
-            If shape Is Nothing Then
-                Return
-            End If
+            If shape Is Nothing Then Return
             e.DefaultDraw()
             Dim imagePath = If(shape.Status = Status.Active, "images/actions/apply_16x16.png", "images/actions/cancel_16x16.png")
             Dim image = DevExpress.Images.ImageResourceCache.Default.GetImage(imagePath)
@@ -52,8 +44,8 @@ Namespace XtraDiagram.CustomDraw
     Public Class DiagramShapeEx
         Inherits DiagramShape
 
-        <XtraSerializableProperty, Category("Info")> _
-        Public Property Status() As Status
+        <XtraSerializableProperty, Category("Info")>
+        Public Property Status As Status
     End Class
 
     Public Enum Status
